74ae52a40151a452ec7108026a372d882f1fa234
fsanntiago <zerofabricio@hotmail.com>
2022-04-13 14:50:32 -0300

    refactor Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,12 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, Platform } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Platform,
+  FlatList,
+} from 'react-native';
 
 import { Button } from '../components/Button';
 import { SkillCard } from '../components/SkillCard';
@@ -21,14 +28,13 @@ export function Home() {
         placeholderTextColor="#555"
         onChangeText={setNewSkill}
       />
-
       <Button onPress={handleAddNewSkill} />
-
       <Text style={[styles.title, { marginVertical: 50 }]}>My Skills</Text>
-
-      {mySkills.map(skill => (
-        <SkillCard skill={skill} />
-      ))}
+      <FlatList
+        data={mySkills}
+        keyExtractor={item => item}
+        renderItem={({ item }) => <SkillCard skill={item} />}
+      />
     </View>
   );
 }
